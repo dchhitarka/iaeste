089e3768a2b9956c195935a87bb0cbc0a50fcdf7
dchhitarka <dchhitarka@gmail.com>
2019-03-07 22:40:34 +0530

    Updated Page

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ else {
 });
 let up = document.querySelector('.up')
 let scroll = up.offsetTop;
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+if (w <= 900px){
+    // let about = document.getElementBy
+}
